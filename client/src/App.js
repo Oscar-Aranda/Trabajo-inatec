b69599b0faadbbdc856042ce585baa3c814f4dbc
Oscar_Aranda <oa97738@gmail.com>
2023-06-03 13:27:22 -0600

    subimo el proyecto

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header >
+        <h1>
+          Aqui es donde vamos a trabajar
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
